485287c986c7f7743e5fb23447507fa99d4ae146
AdamLoginov <mysnikov2000@mail.ru>
2023-11-17 00:00:22 +0300

    commit 2

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -38,6 +38,7 @@ const Form = () =>{
 
         setFormData({
             phone: '',
+            contactPhone: '',
             firstName: '',
             lastName: '',
             surName: '',
@@ -68,6 +69,12 @@ const Form = () =>{
             <div className="mb-6">
                 <label for="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Номер телефона</label>
                 <input type="text" name='phone' value={formData.phone} onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
+                    focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                    dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" required />
+            </div>
+            <div className="mb-6">
+                <label for="contactPhone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Контактный номер телефона</label>
+                <input type="text" name='contactPhone' value={formData.contactPhone} onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
                     focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
                     dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="123-45-678" required />
             </div>
